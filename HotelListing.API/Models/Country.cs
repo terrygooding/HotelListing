84bcef7f75e2580df25ca0b0cd83d16f628921c6
Terry Gooding <terry.gooding@cefas.co.uk>
2022-12-15 23:11:15 +0000

    Added Entity Framework and Created Database.

---
+++ HotelListing.API/Models/Country.cs
@@ -0,0 +1,11 @@
+﻿namespace HotelListing.API.Models
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ShortName { get; set; }
+
+        public virtual IList<Hotel> Hotels { get; set; }
+    }
+}
